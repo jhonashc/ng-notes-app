019a7fbb33fca9d8c18a9c528fa6d24efa28d28e
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-08-13 14:55:52 -0500

    shared model added: navbar component added

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [RouterModule],
+  exports: [NavbarComponent],
+})
+export class SharedModule {}
